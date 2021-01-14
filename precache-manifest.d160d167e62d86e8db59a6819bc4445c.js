801b1655f34c660bfb1f48049aa3da5367704413
Natalia Danilchanka <nataladanilcenko@gmail.com>
2021-01-14 12:14:06 +0300

    Updates

---
+++ precache-manifest.d160d167e62d86e8db59a6819bc4445c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "875516a6a5d27d75cf2b7f64e9601e8d",
+    "url": "/gornadan/SocialNetwork/index.html"
+  },
+  {
+    "revision": "c3bb499ea2e9b11e4854",
+    "url": "/gornadan/SocialNetwork/static/css/main.24eb1020.chunk.css"
+  },
+  {
+    "revision": "bc1a3df8acaadf2c73a4",
+    "url": "/gornadan/SocialNetwork/static/js/2.988e6a88.chunk.js"
+  },
+  {
+    "revision": "3c73c585782ac05880c0f89bcfdbba5a",
+    "url": "/gornadan/SocialNetwork/static/js/2.988e6a88.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3bb499ea2e9b11e4854",
+    "url": "/gornadan/SocialNetwork/static/js/main.143887af.chunk.js"
+  },
+  {
+    "revision": "82889300d762a7bdec17",
+    "url": "/gornadan/SocialNetwork/static/js/runtime-main.9afaa3a3.js"
+  }
+]);
